8cdbf52fdc630466c72455a03df27387618247f5
Otter <teo2005@bk.ru>
2024-02-29 20:50:10 +0300

    connecteing to the api MedicMadlab

---
+++ ConnectAPI/Models/Catalogs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectAPI.Models
+{
+    /// <summary>
+    /// Для доступа к Catalogs класс обязательно должен быть public
+    /// Переписываем со схемы свойства для всех полей с большой буквы и public
+    /// </summary>
+    public class Catalogs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Category { get; set; }
+        public string TimeResul { get; set; }
+        public string Preparation { get; set; }
+        public string Bio { get; set; }
+
+    }
+}
---
+++ ConnectAPI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using ConnectAPI.Models;
+using Newtonsoft.Json;
+using ReactiveUI;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ConnectAPI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// При запуске конструктор запускает метод, делающий запрос к БД
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            GetCatalogs();
+        }
+
+        /// <summary>
+        /// Cоздаем поле catalogs
+        /// </summary>
+        private List<Catalogs> catalogs;
+
+        /// <summary>
+        /// Инициализируем свойство для данного поля
+        /// RaiseAndSetIfChanged() обновляет экран, когда изменяет информацию!
+        /// </summary>
+        public List<Catalogs> Catalogs { get => catalogs; set => this.RaiseAndSetIfChanged(ref catalogs, value); }
+
+        /// <summary>
+        /// Task - класс, выполняющий асинхронные операции
+        /// async выполняет метод в асинхронном потоке
+        /// Не забываем указывть await при получении ответа или Content не сработает
+        /// </summary>
+        /// <returns></returns>
+        public async Task GetCatalogs()
+        {
+            // Для начала создаем клиента
+            HttpClient client = new HttpClient();
+            // Отправляем запрос Get через клиента на сервер - await позволяет использовать его ассинхронно
+            var response = await client.GetAsync(@"https://iis.ngknn.ru/NGKNN/%D0%9C%D0%B0%D0%BC%D1%88%D0%B5%D0%B2%D0%B0%D0%AE%D0%A1/MedicMadlab/api/Catalog");
+            //получаем строку
+            var str = response.Content.ReadAsStringAsync().Result;
+            // Выбираем управление пакетами NuGet и устанавливаем в проект Newtonsoft.Json
+            //вызываем свойство Catalogs и даем ему лист, десериализованные из языка Json.
+            Catalogs = JsonConvert.DeserializeObject<List<Catalogs>>(str);
+        }
+    }
+}
